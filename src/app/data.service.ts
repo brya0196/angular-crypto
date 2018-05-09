2b7a245a9f0585fc70937312da06a1d316c3a94f
Bryan Astacio <bryanastacio@MacBook-Pro-de-Bryan.local>
2018-05-08 22:00:21 -0400

    added the crypto dataservice to the app.component and iterated the data form the service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  result: any;
+
+  constructor(private _http: Http) { }
+
+  getPrices() {
+    return this._http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR')
+      .pipe(map( result => this.result = result.json() ))
+  }
 }
